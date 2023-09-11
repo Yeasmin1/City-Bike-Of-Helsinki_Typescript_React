546125d18ac12c4d6df2d64a812d993f0f79b6cd
Farida Yeasmin <sohanamou25@yahoo.com>
2023-09-11 14:44:17 +0300

    refactored first navigation bar and also  added logout button in it

--- src/container/Navigation.tsx
+++ src/container/Navigation.tsx
@@ -1,123 +1,166 @@
-import { Link } from "react-router-dom";
-import { LANGUAGES } from "./Language";
-import { NavLink } from "react-router-dom";
+import React, { Fragment, useState} from 'react';
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { googleLogout} from '@react-oauth/google';
 import { BsPersonCircle } from 'react-icons/bs';
+import { LANGUAGES } from "./Language";
+import Dropdown from 'react-bootstrap/Dropdown';
 
-interface loginProfileType{
-   loginProfile: any; 
+interface ProfileType{
+    picture?: string;
+    name: string;
+    email?: string;
    }
+   
+interface loginProfileType{
+   loginProfile: any | ProfileType;
+   setLoginProfile: any;
+}
 
-const isActive = ({ isActive }: any) => `link ${isActive ? "active" : ""}`;
-const Navigation:React.FC<loginProfileType>= ({loginProfile})  => {
-    const { i18n, t } = useTranslation();
-    const onClickLang = (e: React.MouseEvent<HTMLButtonElement>) => {
-            const lang_code = e.currentTarget.value
-            i18n.changeLanguage(lang_code);
+const Navigation:React.FC<loginProfileType>= ({loginProfile, setLoginProfile})  => {
+    const [open, setOpen] = useState(false);
+    const {i18n, t} = useTranslation();
+    const navigate = useNavigate();
+
+
+    const handleClickLang = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const lang_code = e.currentTarget.value
+        i18n.changeLanguage(lang_code);
     };
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+      
+    const handleNavigate = ()=>{
+        navigate("/loginForm") ;
+    };
+
+    const handleLogout = () =>{
+        googleLogout();
+        setLoginProfile(null);
+        setOpen(false);
+        window.sessionStorage.clear();
+        navigate("/") ;
+    };
+    //TODO: error handling for googleLogout
 
     return(
         <div >
-            <nav className="navbarFirst">
-             <div className="container">
-             <div className="nav-elements">
-                <ul>
-                <li>
-                    <div>
-                        {LANGUAGES.map(({ code, label }) => (
-                            <button className="btn-custom-button ml-1" value={code} onClick={onClickLang}>
-                            {label}
-                            </button>
-                            ))}
-                    </div>
-                    </li>
-                    <li>
-                    <div>
-                    <NavLink to="/loginForm"> {loginProfile ? (
-                        <div>{loginProfile.name}</div>    
-                        ) : (
-                            <button className="btn-custom-button profile-custom">
-                                <span >
-                           <BsPersonCircle/> &nbsp;Login
-                         </span>
+            <nav id="navbar-first">
+                <div className="container">
+                <div className="nav-elements ">
+                    <ul>
+                        <li>
+                        <div>
+                            { /* Fragment is required to define unique keys for each item
+                             because JSX elements directly inside a map() call always need key */
+                            LANGUAGES.map(languageItem => 
+                                <Fragment key={languageItem.id}> 
+                            
+                                <button className='btn-custom-button ml-1' value={languageItem.code} onClick={handleClickLang}>
+                                    {languageItem.label}
+                                </button>
 
-                            </button>
-                        
-                        )}</NavLink>
-                          </div>
-                    </li>
-                    
-                </ul>
-        </div>
-      </div>
-    </nav>
+                                
+                                </Fragment>  
+                            )
+                            }
+                        </div>
+                        </li>
+                        <li> 
+                            {
+                            loginProfile ? (
+                                <div>
+                                    {/*<Dropdown className='drop'>
+                                    <Dropdown.Toggle className='btn-custom-button' id="dropdown-basic">
+                                    {loginProfile.name} 
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu className='dropMenu'>
+                                        <Dropdown.Item  onClick={handleLogout} href="#/">Logout</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                       
+                                    </Dropdown>
+                                    */}
+                                   <button className='btn-custom-button' onClick={handleOpen}>
+                                        {loginProfile.name}   
+                                    </button>  
+                                    {
+                                    open ? (
+                                        <div >
+                                        <button className='logout-button 'onClick={handleLogout}>
+                                            Logout
+                                        </button>
+                                        </div>
+                                    ) : null
+                                    }
+                              
+                                </div>
+                            ) : (
+                                <div>
+                                    <button className='btn-custom-button 'onClick={handleNavigate }>
+                                        <span className='span-button'>
+                                         <BsPersonCircle /> &nbsp;Login
+                                        </span>
+                                        
+                                    </button> 
+                                </div>
+                            )
+                            }
+                        </li>
+                     </ul>
+                </div>
+                </div>
+            </nav>
    
-      
-     <nav id='menu' className='navbar-default '>
-             
-        <div className='container'>
-            <div className='navbar-header'>
+            <nav id='menu' className='navbar-default '>
+                <div className='container'>
+                <div className='navbar-header'>
                 <button
                     type='button'
                     className='navbar-toggle collapsed'
                     data-toggle='collapse'
-                    data-target='#bs-example-navbar-collapse-1'
-                >
+                    data-target='#navbar-item-collapse'>
                     {' '}
-                    <span className='sr-only'>Toggle navigation</span>{' '}
+                    <span className='sr-only'></span>{' '}
                     <span className='icon-bar'></span>{' '}
                     <span className='icon-bar'></span>{' '}
                     <span className='icon-bar'></span>{' '}
                 </button>
-                
+
                 <Link className='navbar-brand page-scroll' to={{pathname:"/"}}>
                     CBH
                 </Link>
-                
-            </div>
-            
-            <div
-            className='collapse navbar-collapse'
-            id='bs-example-navbar-collapse-1'
-            >
-            <ul className='nav navbar-nav navbar-right'>
-                
-            <li>
-                <NavLink className={isActive} to="/bikeStation">
-                {t("bikeStation")}
-            </NavLink>
+                </div>
 
-            </li>
-
-                <li>
-                <NavLink className={isActive} to="/ticketsInfo">
-                {t("ticketsInfo")}
-                </NavLink>
-            
-                </li>
-
-                <li>
-                <NavLink className={isActive} to="/travelling">
-                {t("travelling")}
-                </NavLink>
-                </li>
-                
-                <li>
-                <NavLink className={isActive} to="/customer">
-                {t("customer")}
-                
-                </NavLink>
-                </li>
-                
-            </ul>
-            
-            </div>
+                <div className='collapse navbar-collapse' id='navbar-item-collapse'>
+                <ul className='nav navbar-nav navbar-right'>
+                    <li>
+                    <NavLink to="/bikeStation">
+                        {t("bikeStation")}
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/ticketsInfo">
+                    {t("ticketsInfo")}
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/">
+                    {t("travelling")}
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to="/">
+                    {t("customer")}
+                    </NavLink>
+                    </li>
+                </ul>
+                </div>
+                </div>
+            </nav>
         </div>
-           
-           
-
-    </nav>
-    </div>
     
     );
 }
